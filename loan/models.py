dd49e9218f4694a6e55eb932147b58b70a497426
Ian Yu-Hsun Lin <ianyuhsunlin@gmail.com>
2015-07-23 15:29:56 +0100

    add models

--- loan/models.py
+++ loan/models.py
@@ -3,26 +3,37 @@ from django.core.validators import MinValueValidator
 from django.core.validators import MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+import const
+
+BUSINESS_SECTOR = []
+for b in const.BUSINESS_SECTOR:
+    BUSINESS_SECTOR.append((const.BUSINESS_SECTOR[b], b))
 
 class Borrower(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+    user = models.ForeignKey(User, null=True)
     name = models.CharField(max_length=200)
     email = models.EmailField()
     phone = PhoneNumberField()
 
 class Business(models.Model):
 
-    RETAIL = 'RE'
-    PROFESSIONAL_SERVICES = 'PS'
-    FOOD = 'FOOD'
-    ENTERTAINMENT = 'ENT'
+    def __str__(self):
+        return self.company_name
 
-    BUSINESS_SECTOR = (
-        (RETAIL, 'retail'),
-        (PROFESSIONAL_SERVICES, 'Profession Services'),
-        (FOOD, 'Food and Drink'),
-        (ENTERTAINMENT, 'Entertainment')
-    )
+    def __unicode__(self):
+        return self.company_name
+
+    def owner_name(self):
+        return self.owner.name
 
     owner = models.ForeignKey(Borrower)
     company_name = models.CharField(max_length=200)
@@ -31,6 +42,16 @@ class Business(models.Model):
     business_sector = models.CharField(max_length=4, choices=BUSINESS_SECTOR)
 
 class Loan(models.Model):
+
+    def __str__(self):
+        return '{0} : {1}'.format(self.business.company_name, self.amount)
+
+    def __unicode__(self):
+        return '{0} : {1}'.format(self.business.company_name, self.amount)
+
+    def company_name(self):
+        return self.business.company_name
+
     business = models.ForeignKey(Business)
     amount = models.PositiveIntegerField(validators=[MinValueValidator(10000),
                                                      MaxValueValidator(100000)])
